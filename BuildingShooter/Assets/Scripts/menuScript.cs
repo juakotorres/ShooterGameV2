f7bbee4a7b7004fcfde4a67d1e7b3029aab55817
juakotorres <juako.torres@hotmail.com>
2017-01-19 22:09:53 -0300

    simple menu

---
+++ BuildingShooter/Assets/Scripts/menuScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class menuScript : MonoBehaviour {
+
+	public Canvas quitMenu;
+	public Button startText;
+	public Button exitText;
+
+	// Use this for initialization
+	void Start () {
+		/* for initialize the button components 	*/
+		quitMenu = quitMenu.GetComponent<Canvas> ();
+		startText = startText.GetComponent<Button> ();
+		exitText = exitText.GetComponent<Button> ();
+		quitMenu.enabled = false;
+	}
+	
+	public void ExitPress(){
+		quitMenu.enabled = true;
+		startText.enabled = false;
+		exitText.enabled = false;
+	}
+
+	public void NoPress(){
+		quitMenu.enabled = false;
+		startText.enabled = true;
+		exitText.enabled = true;
+	}
+
+	public void startLevel(){
+		SceneManager.LoadScene ("MyScene");
+	}
+
+	public void exitGame(){
+		Application.Quit ();
+	}
+}
