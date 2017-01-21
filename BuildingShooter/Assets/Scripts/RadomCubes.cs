b1d799e3841c7cf77e3e7eadbc1d8ed0b8936fe1
Felipe Olivares <Felipe Olivares>
2017-01-20 23:24:54 -0300

    random cubes

---
+++ BuildingShooter/Assets/Scripts/RadomCubes.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadomCubes : MonoBehaviour {
+
+	public GameObject Cubeprefab;
+
+	public Vector3 center;
+	public Vector3 size;
+	public float spawns;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < spawns; i++) {
+			Spawn ();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*if (Input.GetKey (KeyCode.Q)) {
+			Spawn ();
+		}*/
+	}
+
+	public void Spawn(){
+
+		Vector3 pos = center + new Vector3 (Random.Range (-size.x / 2, size.x / 2), Random.Range (-size.y / 2, size.y / 2), Random.Range (-size.z / 2, size.z / 2));
+		
+		Instantiate (Cubeprefab, pos, Quaternion.identity);
+	}
+
+	void OnDrawGizmosSelected(){
+
+		Gizmos.color = new Color (1,0,0,0.5f);
+		Gizmos.DrawCube (center, size);
+
+	}
+}
