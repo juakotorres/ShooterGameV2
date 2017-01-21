3b3e2e9ea3b699dfe574017900b7b10fb077827b
Americo Ferrada <Americo Ferrada>
2017-01-21 00:03:51 -0300

    luciernaga

---
+++ BuildingShooter/Assets/LightManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightManager : MonoBehaviour {
+
+    public Light lucesita;
+    public GameObject  imagen;
+    private SpriteRenderer render;
+
+	// Use this for initialization
+	void Start () {
+        render = imagen.GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.X))
+            TurnOn();
+
+        if (Input.GetKey(KeyCode.Z))
+            TurnOff();
+    }
+
+
+    public void TurnOn()
+    {
+        render.enabled = true;
+        lucesita.intensity = 0.1F;
+    }
+
+
+    public void TurnOff()
+    {
+        render.enabled = false;
+        lucesita.intensity = 0.00F;
+    }
+}
